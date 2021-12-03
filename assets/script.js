ee7bf8faea89b37d3eb0368833901fd30bdf2fb8
Kelsey Bassanini <KelseyRCIS@gmail.com>
2021-12-02 21:23:05 -0800

    Updated the Cookie quantities

--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,116 @@
 console.log('JavaScript code has loaded!')
 
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
-
-// We'll use these variables to track the counts of each cookie type
-let gb = 0      // Gingerbread
-let cc = 0      // Chocolate Chip
-let sugar = 0   // Sugar Sprinkle
+let yourName = "Kelsey Bassanini"
 
 // Code to update name display 
 document.getElementById('credit').textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
-document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    console.log('Gingerbread + button was clicked!')
+// Updates cookie quantity - select elements by ID
+//function updateQuantity(displayQuantity){
+//   let quantity = document.querySelector('#qty-gb')
+//   quantity.innerHTML = displayQuantity
+//}
 
-    // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+// Start quantity and variables to track the counts of each cookie type
+let gb = 0      // Gingerbread
+let cc = 0      // Chocolate Chip
+let sugar = 0   // Sugar Sprinkle
+
+// Buttons - input elements
+//const minusGb = document.querySelector('#minus-gb')
+//const addGb = document.querySelector('#add-gb')
+//const minusCc = document.querySelector('#minus-cc')
+//const addCc = document.querySelector('#add-cc')
+//const minusSugar = document.querySelector('#minus-sugar')
+//const addSugar = document.querySelector('#add-sugar')
+
+// Event listener for clicks on the "+" button for Gingerbread cookies
+document.getElementById('add-gb').addEventListener('click', function(e) {
+    gb++;
+    document.getElementById('add-gb').textContent=gb;
+    document.getElementById('qty-total').textContent=gb+cc+sugar;
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+// Event listener for clicks on the "-" button for Gingerbread cookies
+document.getElementById('minus-gb').addEventListener('click', function(e) {
+        gb--;
+        document.getElementById('minus-gb').textContent=gb;
+        document.getElementById('qty-total').textContent=gb+cc+sugar;
+})
+
+// Event listener for clicks on the "+" button for Chocolate Chip cookies
+document.getElementById('add-cc').addEventListener('click', function(e) {
+    cc++;
+    document.getElementById('add-cc').textContent=gb;
+    document.getElementById('qty-total').textContent=gb+cc+sugar;
+})
+
+// Event listener for clicks on the "-" button for Chocolate Chip cookies
+document.getElementById('minus-cc').addEventListener('click', function(e) {
+        cc--;
+        document.getElementById('minus-cc').textContent=gb;
+        document.getElementById('qty-total').textContent=gb+cc+sugar;
+})
+
+// Event listener for clicks on the "+" button for Sugar Sprinkle cookies
+document.getElementById('add-sugar').addEventListener('click', function(e) {
+    sugar++;
+    document.getElementById('add-sugar').textContent=gb;
+    document.getElementById('qty-total').textContent=gb+cc+sugar;
+})
+
+// Event listener for clicks on the "-" button for Sugar Sprinkle cookies
+document.getElementById('minus-sugar').addEventListener('click', function(e) {
+        sugar--;
+        document.getElementById('minus-sugar').textContent=gb;
+        document.getElementById('qty-total').textContent=gb+cc+sugar;
+})
+
+// // Event listener for clicks on the "+" button for Gingerbread cookies
+// document.getElementById('add-gb').addEventListener('click', function(e) {
+//     quantity++
+//     updateQuantity(`Quantity: ${quantity}`)
+//     // HINT: You can delete this console.log after you no longer need it!
+//     console.log('Gingerbread + button was clicked!')
+// })
+
+// // TODO: Hook up event listeners for the rest of the buttons
+
+// // Event listener for clicks on the "-" button for Chocolate Chip cookies
+// document.getElementById('minus-gb').addEventListener('click', function(e) {
+//     if (quantity >0) {
+//         quantity--
+//     }
+//     updateQuantity(`Quantity: ${quantity}`)
+//     // HINT: You can delete this console.log after you no longer need it!
+//     console.log('Chocolate Chip + button was clicked!')
+// })
+
+// // Event listener for clicks on the "+" button for Chocolate Chip cookies
+// addCc.getElementById('add-gb').addEventListener('click', function(e) {
+//     quantity++
+//     updateQuantity(`Quantity: ${quantity}`)
+//     // HINT: You can delete this console.log after you no longer need it!
+//     console.log('Chocolate Chip + button was clicked!')
+// })
+
+// // Event listener for clicks on the "-" button for Sugar Sprinkles cookies
+// minusSugar.getElementById('minus-gb').addEventListener('click', function(e) {
+//     if (quantity >0) {
+//         quantity--
+//     }
+//     updateQuantity(`Quantity: ${quantity}`)
+//     // HINT: You can delete this console.log after you no longer need it!
+//     console.log('Sugar Sprinkle + button was clicked!')
+// })
+
+// // Event listener for clicks on the "+" button for Sugar Sprinkles cookies
+// addSugar.getElementById('add-gb').addEventListener('click', function(e) {
+//     quantity++
+//     updateQuantity(`Quantity: ${quantity}`)
+//     // HINT: You can delete this console.log after you no longer need it!
+//     console.log('Sugar Sprinkle + button was clicked!')
+// })
+
+
